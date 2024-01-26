17ae46bf9a4e489eec2ce4756cd3e095d92d90c8
Shaheen Karodia <karodia.shaheen@gmail.com>
2024-01-26 11:33:54 +0200

    updated formatting

--- react/components/HeadlessLink.tsx
+++ react/components/HeadlessLink.tsx
@@ -1,26 +1,28 @@
-import React from 'react'
-import HeadlessLoadingBar from './HeadlessLoadingBar'
+import React from 'react';
+import HeadlessLoadingBar from './HeadlessLoadingBar';
 
 type IProps = {
-    children: React.ReactNode
-    className?: string
-    onClick?: () => void
-    href?: string
-}
-function HeadlessLink(props: IProps) {
-	const [showLoadingBar, setShowLoadingBar] = React.useState(false);
+  children: React.ReactNode;
+  className?: string;
+  onClick?: () => void;
+  href?: string;
+};
 
-	const handleClick = () => {
-		setShowLoadingBar(true)
+function HeadlessLink(props: IProps) {
+  const [showLoadingBar, setShowLoadingBar] = React.useState(false);
+
+  const handleClick = () => {
+    setShowLoadingBar(true);
   };
 
   return (
     <>
-     {showLoadingBar && <HeadlessLoadingBar /> } 
-     	<a href={props.href} onClick={handleClick} className={props.className}>{props.children}</a>
+      {showLoadingBar && <HeadlessLoadingBar />}
+      <a href={props.href} onClick={handleClick} className={props.className}>
+        {props.children}
+      </a>
     </>
-   
-  )
+  );
 }
 
-export default HeadlessLink
+export default HeadlessLink;
